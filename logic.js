14cf48f33169b922643bab016e7d156649c2a16d
Fernando Ubuntu <darkblitzrc@gmail.com>
2022-05-24 22:58:25 -0500

    Remove game() function and commented out function
    So did quite some stuff here! First, I removed the game() function because it was just there sitting and doing nothing since we are not using the console anymore!
    
    Next, I removed the commented out code for the function which worked with the buttons!
    
    Finally, hit a bit of a block road but it was all easy to fix. INitially the challenge was adding a way to identify each image with a value, let this value be either 'Rock', 'Paper' or 'scissors'. The solution was simply to add an ID to each image and then target it using event.target.id.

--- logic.js
+++ logic.js
@@ -40,24 +40,6 @@ function playRound(playerSelection, computerSelection){
 let player_score = 0
 let computer_score = 0
 
-function game(){
-    player_score = 0
-    computer_score = 0
-    rounds = parseInt(prompt('Please input the number of rounds you would like to play.'));
-    for (let i = 0; i < rounds; i++){
-        playerSelection = prompt('Please type: \'Rock\', \'Paper\', \'Scissors\' to play!').toLowerCase();
-        computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
-    }
-    if (player_score > computer_score){
-        return 'You won! Your score was: ' + player_score;
-    } else if (computer_score > player_score){
-        return 'You lost your score was ' + player_score + ' but the computer score was ' + computer_score;
-    } else{
-        return 'It\'s a tie between you and the computer, Final Score: Player ' + player_score + ', Computer ' + computer_score;
-    }
-}
-
 //DOM methods
 
 let currentRound = document.querySelector('.current-round');
@@ -68,56 +50,33 @@ let cDraw = document.querySelector('.computer-draw')
 
 let roundResult = document.querySelector('.round-result');
 
-let buttons = document.getElementsByClassName('button');
+let choices = document.getElementsByClassName('choice');
 
-let buttonsList = Array.from(buttons);
-
-let test = document.querySelector('.rock')
-
-test.addEventListener('click', () =>{
-    type = 'rock'
-    playRound(type,computerPlay());
-        pScore.textContent = 'Your score is: ' + player_score;
-        currentRound.textContent = 'Round ' + ++round;
-        cDraw.textContent = computerDraw;
-        roundResult.textContent = result;
-        if (round == 5){
-            cDraw.textContent = computerDraw;
-            if (player_score > computer_score){
-                roundResult.textContent = 'YOU WON THE GAME! Your score was: ' + player_score;
-            } else if (computer_score > player_score){
-                roundResult.textContent = 'YOU LOST THE GAME! Computer Score was: ' + computer_score;
-            } else{
-                roundResult.textContent =  'It\'s a tie between you and the computer!'
-            }
-            player_score = 0
-            computer_score = 0
-            round = 0  
-        }
-    
-})
+let choicesList = Array.from(choices)
 
 let round = 0
 
-// buttonsList.forEach(button =>{
-//     button.addEventListener('click', (e)=>{
-//         playRound(e.target.innerText.toLowerCase(),computerPlay());
-//         pScore.textContent = 'Your score is: ' + player_score;
-//         currentRound.textContent = 'Round ' + ++round;
-//         cDraw.textContent = computerDraw;
-//         roundResult.textContent = result;
-//         if (round == 5){
-//             cDraw.textContent = computerDraw;
-//             if (player_score > computer_score){
-//                 roundResult.textContent = 'YOU WON THE GAME! Your score was: ' + player_score;
-//             } else if (computer_score > player_score){
-//                 roundResult.textContent = 'YOU LOST THE GAME! Computer Score was: ' + computer_score;
-//             } else{
-//                 roundResult.textContent =  'It\'s a tie between you and the computer!'
-//             }
-//             player_score = 0
-//             computer_score = 0
-//             round = 0  
-//         }
-//     });
-// });
+choicesList.forEach(choice => {
+    choice.addEventListener('click', (e) =>{
+        playRound(e.target.id,computerPlay());
+            pScore.textContent = 'Your score is: ' + player_score;
+            currentRound.textContent = 'Round ' + ++round;
+
+            cDraw.textContent = computerDraw;
+            roundResult.textContent = result;
+            if (round == 5){
+                cDraw.textContent = computerDraw;
+                if (player_score > computer_score){
+                    roundResult.textContent = 'YOU WON THE GAME! Your score was: ' + player_score;
+                } else if (computer_score > player_score){
+                    roundResult.textContent = 'YOU LOST THE GAME! Computer Score was: ' + computer_score;
+                } else{
+                    roundResult.textContent =  'It\'s a tie between you and the computer!'
+                }
+                player_score = 0
+                computer_score = 0
+                round = 0  
+            }
+        
+    })
+})
